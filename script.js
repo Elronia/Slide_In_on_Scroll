c7c40b8a0721388535deb485743315ee3e221772
Elronia <zar.catherine@gmail.com>
2021-03-20 23:55:19 -0400

    looping over sliderImages and finishing the logic for checkSlide function

--- script.js
+++ script.js
@@ -16,7 +16,20 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in');
 
 function checkSlide(e) {
-    console.count(e);
+    // console.log(window.scrollY);
+    sliderImages.forEach(sliderImage => {
+        // half way throught the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        // bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
 }
 
 window.addEventListener('scroll', debounce(checkSlide));
